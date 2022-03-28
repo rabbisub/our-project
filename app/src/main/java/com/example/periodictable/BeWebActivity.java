41d4aeb64bd5da1bd31965c3fb65de3477097e01
rabbisub <rabbi.cse.56005@sub.ac.bd>
2022-03-29 01:01:32 +0600

    Rabbi

---
+++ app/src/main/java/com/example/periodictable/BeWebActivity.java
@@ -0,0 +1,14 @@
+package com.example.periodictable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeWebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_be_web);
+    }
+}
