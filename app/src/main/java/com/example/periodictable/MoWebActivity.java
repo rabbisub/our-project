54067367bff1f17c0086127c6d0f544d99133813
rabbisub <rabbi.cse.56005@sub.ac.bd>
2022-04-24 19:26:09 +0600

    Rabbi

---
+++ app/src/main/java/com/example/periodictable/MoWebActivity.java
@@ -0,0 +1,14 @@
+package com.example.periodictable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoWebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mo_web);
+    }
+}
---
+++ app/src/main/java/com/example/periodictable/TcWebActivity.java
@@ -0,0 +1,14 @@
+package com.example.periodictable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TcWebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tc_web);
+    }
+}
